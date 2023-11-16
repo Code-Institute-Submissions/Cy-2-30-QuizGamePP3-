b727125587d4960cd9063927d09ff615fd133df8
Cy-2-30 (Dee) <lebulicynthia@gmail.com>
2023-11-16 21:59:45 +0000

    add Google Sheets APIs

--- run.py
+++ run.py
@@ -10,7 +10,7 @@ SCOPE = [
 CREDS = Credentials.from_service_account_file('creds.json')
 SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
-SHEET = GSPREAD_CLIENT.open('pp3_quiz')
+SHEET = GSPREAD_CLIENT.open('quizgamepp3')
 
 teams = SHEET.worksheet('teams')
 records = teams.get_all_values()
